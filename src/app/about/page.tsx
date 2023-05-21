839c0018745ba49d57019c1435e47f39bfc36931
mojtabafazeli <fazeli.mojtaba@gmail.com>
2023-05-21 19:06:13 +0000

    adds prov names

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function About() {
+  throw new Error('error');
   return (
     <div>about</div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+'use client';
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import { useState, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [allProvs, setAllProvs] = useState([]);
+  // console.log('all provs', allProv);
+  useEffect(() => {
+    fetch('https://iran-locations-api.vercel.app/api/v1/states')
+    .then(res => res.json())
+    .then(res => setAllProvs(res));
+  }, [])
+
+  const provs = allProvs?.map(prov => (
+    <li key={prov.id}>
+      <a href={prov.name}>{prov.name}</a>
+      </li>
+  ))
   return (
-    <main className={styles.main}>
-      <h1>Hello Next!</h1>
+    <main>
+      <ul>
+      <label>فهرست استان ها</label>
+      {provs}
+      </ul>
     </main>
   )
 }
