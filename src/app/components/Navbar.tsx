32a75c89375dff61d9214b12f2c145e9d3f2b06b
mojtaba <user@mysite.com>
2023-07-02 09:22:29 -0400

    fix home link

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
       <nav className="bg-slate-600 p-4 sticky drop-shadow-xl z-10">
           <div className="prose prose-xl mx-auto flex gap-6">
-          <Link href="/" className="flex items-center gap-1 text-white/90 no-underline hover:text-white"><FaHome />خانه</Link>
+          <Link href="/home" className="flex items-center gap-1 text-white/90 no-underline hover:text-white"><FaHome />خانه</Link>
           <Link href="/province" className="flex items-center gap-1 text-white/90 no-underline hover:text-white"><FaCity />استان ها</Link>
       </div>
     </nav>
